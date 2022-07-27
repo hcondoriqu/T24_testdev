f1bd734ea83a14d4a6738ea02125a53ed378285e
root <root@DESKTOP-28H3L15.localdomain>
2022-07-27 14:26:36 -0400

    debug of ADC 7291

--- rpi_prev/adc_read.py
+++ rpi_prev/adc_read.py
@@ -13,13 +13,12 @@ bus = smbus.SMBus(1)  # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 
 def get_adc():
 
-    reg_sel = 0x1  # Voltage conversion result register
+    reg_sel = 0x3  # Voltage conversion result register
     adc_i2c_address = 0x2F
     i2c_bus = smbus.SMBus(1)  # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
     data_read = i2c_bus.read_i2c_block_data(adc_i2c_address, reg_sel)
     print("Data read binary: ", data_read)
     data_msb = data_read[0]
-
     data_lsb = data_read[1]
 
     data_comb = data_msb | data_lsb
@@ -37,11 +36,8 @@ def write_reg(ch_dac):
 
     register_address = 0x0
     ## address pointing register
-    data_1 = 0x0  ## points to the command register
-    data_msb = 0x1  ## Ch0
-    data_lsb = 0x0  ## configuration register
-    data_array = [data_1, data_msb, data_lsb]
-    bus.write_i2c_block_data(adc_i2c_address, register_address, data_array)
+
+    bus.write_word_data(adc_i2c_address, register_address, 0xA000)
 
 
 def get_data():
