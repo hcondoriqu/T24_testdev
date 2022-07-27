9a8540f519b9f461e38cc742d00e01a4f4e35ca2
root <root@DESKTOP-28H3L15.localdomain>
2022-07-27 11:26:22 -0400

    adding ADC script

--- rpi_prev/adc_read.py
+++ rpi_prev/adc_read.py
@@ -11,25 +11,25 @@ import RPi.GPIO as GPIO
 bus = smbus.SMBus(1)  # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 
 
-def get_adc():
+def get_adc(reg):
 
-    Reg_SEL = 0x00  # ADC Register
+    reg_sel = reg  # ADC Register
     adc_i2c_address = 0x2F
-    bus = smbus.SMBus(1)  # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+    i2c_bus = smbus.SMBus(1)  # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 
-    Reg_SEL = 0x00  # Temperature Register
+    reg_sel = 0x00  # Temperature Register
 
-    Data_read = bus.read_i2c_block_data(adc_i2c_address, Reg_SEL)
-    Data_MSB = Data_read[0] << 8
-    Data_LSB = Data_read[1]
-    Data_comb = Data_MSB | Data_LSB
-    adc_bin = Data_comb >> 5  # change to 11bit
+    data_read = i2c_bus.read_i2c_block_data(adc_i2c_address, reg_sel)
+    data_msb = data_read[0] << 8
+    data_lsb = data_read[1]
+    data_comb = data_msb | data_lsb
+    adc_bin = data_comb >> 5  # change to 11bit
 
     # check negative temp bit status
-    Neg_data_check = adc_bin & 0b10000000000
-    Neg_bit_check = Neg_data_check >> 10
+    neg_data_check = adc_bin & 0b10000000000
+    neg_bit_check = neg_data_check >> 10
 
-    if Neg_bit_check == 0:
+    if neg_bit_check == 0:
         adc_read = adc_bin * 0.125
         # print ("Temp degC: ", temp_read)
 
@@ -42,13 +42,15 @@ def get_adc():
     return adc_read
 
 
-def get_data():
-    data = get_adc()
-    print("ADC reading " + " : " + str(data))
+def get_data(reg):
+    data = get_adc(reg)
+    print("ADC reading " + str(reg) + " : " + str(data))
 
 
 def main():
-    get_data()
+    reg_map = [1, 2, 3, 4, 5, 6, 7, 8]
+    for reg in reg_map:
+        get_data(reg)
 
 
 if __name__ == "__main__":
--- rpi_prev/fpga_rw.py
+++ rpi_prev/fpga_rw.py
@@ -27,106 +27,98 @@ SPI_MISO_PIN = 21
 # Note: internally values shall be passed integers (decimal).
 
 
-
-
 def main():
-  if len(sys.argv) < 3 or len(sys.argv) > 4:
-    sys.exit(usage)
+    if len(sys.argv) < 3 or len(sys.argv) > 4:
+        sys.exit(usage)
 
-  address = int(sys.argv[2],16) # Always hex input, convert to int
-  address = address << 1        # shift address to make room for read bit
+    address = int(sys.argv[2], 16)  # Always hex input, convert to int
+    address = address << 1  # shift address to make room for read bit
 
-  spi_init()
+    spi_init()
 
-  if (sys.argv[1] == "peek"):
-    address |= 1        # Set read bit
-    spi_peek(address)
+    if sys.argv[1] == "peek":
+        address |= 1  # Set read bit
+        spi_peek(address)
 
-  elif (sys.argv[1] == "poke"):
-    write_value = int(sys.argv[3],16) # Always hex input, convert to int
-    spi_poke(address, write_value)
+    elif sys.argv[1] == "poke":
+        write_value = int(sys.argv[3], 16)  # Always hex input, convert to int
+        spi_poke(address, write_value)
 
+    else:
+        sys.exit(usage)
 
-  else:
-    sys.exit(usage)
-
-  sys.exit()
+    sys.exit()
 
 
 def spi_init():
-  GPIO.setmode(GPIO.BOARD)
-  GPIO.setwarnings(False)
-  GPIO.setup([SPI_SS_PIN, SPI_SCLK_PIN, SPI_MOSI_PIN], GPIO.OUT)
-  GPIO.setup([SPI_MISO_PIN], GPIO.IN)
-  GPIO.output(SPI_SS_PIN, GPIO.HIGH)
-  GPIO.output(SPI_SCLK_PIN, GPIO.LOW)
-  GPIO.output(SPI_MOSI_PIN, GPIO.LOW)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
+    GPIO.setup([SPI_SS_PIN, SPI_SCLK_PIN, SPI_MOSI_PIN], GPIO.OUT)
+    GPIO.setup([SPI_MISO_PIN], GPIO.IN)
+    GPIO.output(SPI_SS_PIN, GPIO.HIGH)
+    GPIO.output(SPI_SCLK_PIN, GPIO.LOW)
+    GPIO.output(SPI_MOSI_PIN, GPIO.LOW)
 
 
 def spi_peek(address):
-  # send address (8 bits)
-  # capture 16-bit read value
+    # send address (8 bits)
+    # capture 16-bit read value
 
-  GPIO.output(SPI_SS_PIN, 0)
-  GPIO.output(SPI_SCLK_PIN, 0)
+    GPIO.output(SPI_SS_PIN, 0)
+    GPIO.output(SPI_SCLK_PIN, 0)
 
-  send_byte(address)
-  data = read16()
+    send_byte(address)
+    data = read16()
 
-  GPIO.output(SPI_SS_PIN, 1)
-  address = address >> 1
-  #print "debug:{}".format(data)
-  print ("FPGA READ A:0x{:02x} D:0x{:04x}".format(address, data))
+    GPIO.output(SPI_SS_PIN, 1)
+    address = address >> 1
+    # print "debug:{}".format(data)
+    print("FPGA READ A:0x{:02x} D:0x{:04x}".format(address, data))
 
 
+def spi_poke(address, write_value):
+    # send address (8 bits)
+    upper = write_value >> 8
+    lower = write_value & 255
 
-def spi_poke(address,write_value):
-  # send address (8 bits)
-  upper = write_value >> 8
-  lower = write_value & 255
+    GPIO.output(SPI_SS_PIN, 0)
+    GPIO.output(SPI_SCLK_PIN, 0)
 
-  GPIO.output(SPI_SS_PIN, 0)
-  GPIO.output(SPI_SCLK_PIN, 0)
+    for i in (address, upper, lower):
+        send_byte(i)
 
-  for i in (address,upper,lower):
-    send_byte(i)
+    GPIO.output(SPI_SS_PIN, 1)
+    address = address >> 1
 
-  GPIO.output(SPI_SS_PIN, 1)
-  address = address >> 1
+    print("FPGA WRITE A:0x{:02x} D:0x{:04x}".format(address, write_value))
 
-  print ("FPGA WRITE A:0x{:02x} D:0x{:04x}".format(address,write_value))
 
 def send_byte(value):
 
-  # value is assumed to be decimal string
-  # need to convert to binary list
-  data = list("{0:08b}".format(value))
+    # value is assumed to be decimal string
+    # need to convert to binary list
+    data = list("{0:08b}".format(value))
 
-  for bits in data:
-    GPIO.output(SPI_MOSI_PIN, int(bits)) # express data bit
-    GPIO.output(SPI_SCLK_PIN, GPIO.HIGH) # clock high
-    GPIO.output(SPI_SCLK_PIN, GPIO.LOW)  # clock low
+    for bits in data:
+        GPIO.output(SPI_MOSI_PIN, int(bits))  # express data bit
+        GPIO.output(SPI_SCLK_PIN, GPIO.HIGH)  # clock high
+        GPIO.output(SPI_SCLK_PIN, GPIO.LOW)  # clock low
 
 
 def read16():
-  GPIO.output(SPI_SCLK_PIN, 0)
-  data = []
-
-  for _ in range(16):
-    data.append(GPIO.input(SPI_MISO_PIN))
-    GPIO.output(SPI_SCLK_PIN, 1)
     GPIO.output(SPI_SCLK_PIN, 0)
+    data = []
 
-  read_data = int("".join(map(str,data)),2)
-  #print "read_data={}".format(read_data)
+    for _ in range(16):
+        data.append(GPIO.input(SPI_MISO_PIN))
+        GPIO.output(SPI_SCLK_PIN, 1)
+        GPIO.output(SPI_SCLK_PIN, 0)
 
-  return read_data
+    read_data = int("".join(map(str, data)), 2)
+    # print "read_data={}".format(read_data)
 
-  # Fix
+    return read_data
 
 
-
-
-if __name__ == '__main__':
-   sys.exit(main())
-
+if __name__ == "__main__":
+    sys.exit(main())
