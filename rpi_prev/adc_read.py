b58c7a4b61db9c6500984a5baa3d780036f03966
root <root@DESKTOP-28H3L15.localdomain>
2022-08-18 22:19:48 -0400

    adding adc reading file

--- rpi_prev/adc_read.py
+++ rpi_prev/adc_read.py
@@ -24,17 +24,16 @@ def get_data():
     adc_i2c_address = 0x2F
 
     i2c_bus = smbus.SMBus(1)  # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
-    data_read = i2c_bus.read_word_data(adc_i2c_address, reg_sel)
-    print("Data read binary: ", data_read)
-    data_read2 = i2c_bus.read_i2c_block_data(adc_i2c_address, reg_sel)
-    print("Data read binary: ", data_read2)
-    data_read3 = [hex(h1) for h1 in data_read2]
-    return data_read3
+
+    data_read = bus.read_i2c_block_data(adc_i2c_address, reg_sel,2) # read 2 bytes
+    data_v =  (data_read[0] & 0b00001111 ) * 256 + data_read[1])
+
+    return data_v
 
 
-def write_data(data_w):
+def write_data(ch):
     """Writes into the command register on the ADC"""
-
+    ch_map = {0:0x80,1:0x40,2:0x20,3:0x10}
     bus = smbus.SMBus(1)
     # For the 610-00298 0x2F is the ADC address
     adc_i2c_address = 0x2F
@@ -43,62 +42,27 @@ def write_data(data_w):
     # data to write
     # check 1
 
-    bus.write_word_data(adc_i2c_address, register_address, data_w)
+    bus.write_word_data(adc_i2c_address, register_address, ch_map[ch])
 
 
-def get_adc():
+def get_adc(ch):
     """
     Gets data from the ADC
     1. write into the command register the channel number to read from
     2. Read on the voltage conversion register Address = 0x01
     """
-    p = np.arange(0, 16)
-    # Debug step 1
-
-    data_w = 0x020000
-    write_data(data_w)
+    write_data(ch)
     data = get_data()
-    print("ADC reading " + hex(data_w) + " : " + str(data))
-    # Debug step 2
-
-    for rs in p:
-        # Debug step 3
-
-        data_w = rs
-        write_data(data_w)
-        data = get_data()
-        print("ADC reading" + hex(data_w) + " : " + str(data))
-
-    # Debug step 4
-    data_w = 0x040000
-    write_data(data_w)
-    data = get_data()
-    print("ADC reading" + hex(data_w) + " : " + str(data))
-
-    # Debug step 5
-
-    data_w = 0x0008
-    write_data(data_w)
-    data = get_data()
-    print("ADC reading" + hex(data_w) + " : " + str(data))
-
-    # Debug step 6
-    data_w = 0x080000
-    write_data(data_w)
-    data = get_data()
-    print("ADC reading" + hex(data_w) + " : " + str(data))
-
-    # Debug step 7
-    data_w = 0xA000
-    write_data(data_w)
-    data = get_data()
-    print("ADC reading" + hex(data_w) + " : " + str(data))
+    return data 
+    
+    
+    
 
 
 def main():
-
-    get_adc()
-
+    ch = 0
+    data_read = get_adc(ch)
+    print("data from adc on ch "+str(ch)+" : "+str(data))
 
 if __name__ == "__main__":
     main()
