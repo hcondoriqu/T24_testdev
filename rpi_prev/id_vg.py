dc96813c38ae0cd8ca829506a7da720110330e6c
Hugo Condori <hcondori.contractor@starry.com>
2022-08-18 22:56:01 -0400

    fix bug adc

--- rpi_prev/id_vg.py
+++ rpi_prev/id_vg.py
@@ -5,10 +5,11 @@ import numpy as np
 
 def main():
     ch = 0
+    st_ch = "ch" + str(ch)
     # get drain current vs gate voltage
     vg = np.arange(3, 0.5, -0.1)
     for vdac in vg:
-        set_dac.set_dac(ch, vdac)
+        set_dac.set_dac(st_ch, vdac)
         t = adc_read.get_adc(ch)
         t = t * 200
         print("Drain current ", t)
